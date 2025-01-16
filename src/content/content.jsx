786931b9070c7fe02cabde1aca5096f4f1095856
Edison Matos <edison.matos@live.com>
2025-01-16 11:42:48 -0300

    feat: Corrige textos

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -123,7 +123,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "Atendimento Presencial e Online 24h",
+        text: "Atendimento presencial e online 24h",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
@@ -323,7 +323,7 @@ const content = {
       miniTag: "QUEM É O DR RICHARD MÁRIO",
       title: "Minha trajetória",
       subtitle:
-        "Advogado criminalista com especialidade em execução penal , inscrito nos quadros da ordem dos advogados do Brasil do estado de Santa Catarina (OAB/SC 70972)",
+        "Advogado criminalista com especialização em execução penal, inscrito nos quadros da ordem dos advogados do Brasil do estado de Santa Catarina (OAB/SC 70972)",
       paragraph: (
         <div>
           Formado em 2023 pela Unisul (Universidade do Sul de Santa Catarina),
